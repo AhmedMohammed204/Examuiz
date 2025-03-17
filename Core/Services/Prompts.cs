589b370b65cfeb8fc5836530df6b1fe7d3d19d4e
AhmedMohammed204 <ahmedbkry204@gmail.com>
2025-03-17 20:31:27 +0300

    Enhance create exam prompt

--- Core/Services/Prompts.cs
+++ Core/Services/Prompts.cs
@@ -17,7 +17,7 @@ No need to write introduction or explain anything, just the questions and answer
 IMPORTANT RULES TO FOLLOW:
 - User different HTML Elements for questions.
 - You can use Tables as well to provided specific questions types as needed. 
-- Add some space between questions to allow answers within it. You can use border to tell the student where he can write his answer (just border without any text within it).
+- Add some space between questions to allow answers within it. You can use border on Open ended questions to tell the student where he can write his answer (just border without any text within it).
 - Don't write answers under questions, you can write the answers together as table in the last part of exam.
 - Write the answers as with 2 columns, first column is the question number and the second column is the answer.
 - Write questions in correct order
