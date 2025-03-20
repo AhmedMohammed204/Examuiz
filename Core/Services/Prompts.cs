1c5a66774cbe1df47aba2faa7e5835f6427fc4b8
AhmedMohammed204 <ahmedbkry204@gmail.com>
2025-03-20 16:06:05 +0300

    Update Create Exam Prompt

--- Core/Services/Prompts.cs
+++ Core/Services/Prompts.cs
@@ -20,6 +20,7 @@ IMPORTANT RULES TO FOLLOW:
 - Add some space between questions to allow answers within it. You can use border on Open ended questions to tell the student where he can write his answer (just border without any text within it).
 - Don't write answers under questions, you can write the answers together as table in the last part of exam.
 - Write the answers as with 2 columns, first column is the question number and the second column is the answer.
+- Give answers section class='answers' to be able to find it 
 - Write questions in correct order
 
 The Data:
